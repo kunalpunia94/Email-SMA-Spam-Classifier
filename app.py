874427973a5810069794093a15dbb54e00403c55
Kunal Punia <159790339+kunalpunia94@users.noreply.github.com>
2025-03-15 23:04:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,57 @@
+import streamlit as st
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+import nltk
+nltk.download('punkt')  # Ensure the tokenizer model is downloaded
+from nltk.tokenize import word_tokenize
+
+ps = PorterStemmer()
+
+#preprocess function
+def transform_test(text):
+    text = text.lower()   #1.lower case
+    text = nltk.word_tokenize(text)  #2.Tokenization
+    #till here it is converted into list so we can run the loop and remove the special characters
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    #removing the punctutation
+    text = y[:] #we have to copy like this
+    y.clear()
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    #stemmming
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+    return " ".join(y)
+
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+st.title('Email/SMS Spam Classifier')
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+    #1. Preprocess  ->function written above
+    transformed_sms = transform_test(input_sms)
+    #2. Vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    #3. Predict
+    result = model.predict(vector_input)[0]
+    #4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
+
